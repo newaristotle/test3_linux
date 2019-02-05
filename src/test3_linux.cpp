da4b34167fae75ffcf1453232ea16e39785ec176
root <root@192.168.50.222>
2019-02-06 00:12:36 +0800

    just for linux

---
+++ src/test3_linux.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : test3_linux.cpp
+// Author      : li
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "hahahah" << endl;
+	return 0;
+}
